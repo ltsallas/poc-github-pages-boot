812c9520730682475bd5bd06701d89b1a2dd953c
Lefteris Tsallas <ltsallas@tutanota.com>
2024-11-13 23:41:20 +0200

    Add Dokka plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.jetbrains.dokka") version "1.9.20"
 }
+// Adjust jackson-version from 2.16 for dokka to work. See https://github.com/Kotlin/dokka/issues/3472
+ext["jackson-bom.version"] = "2.15.3"
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
